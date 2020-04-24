98b93cf538c0d0222d3c2643fc774fc06446a2ae
Thudilie <thuyanh.nguyen.de@gmail.com>
2020-04-24 23:03:49 +0200

    Add Routes /Guardian, /Ward

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import './../css/App.css';
 import Guardian from './../routes/Guardian'
 import Ward from './../routes/Ward'
 
 const App = () => {
   return (
-    <>
+    <Router>
       <header>
         header
       </header>
       <main>
-        <Guardian/>
-        <Ward/>
+        <Route exact path="/Guardian" component={Guardian}/>
+        <Route exact path="/Ward" component={Ward}/>
       </main>
       <footer>
         footer
       </footer>
-    </>
+    </Router>
   );
 }
 
