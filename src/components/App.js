bf48442539cd6765572892075792ed839059930b
Thudilie <thuyanh.nguyen.de@gmail.com>
2020-04-25 01:21:04 +0200

    Toggle visibility of components based on token

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useContext} from 'react';
+import React, {useState} from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import './../css/App.css';
--- src/routes/Guardian.js
+++ src/routes/Guardian.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import Auth from './../components/Auth';
+import LoremIpsum from './../components/LoremIpsum';
+import AuthContext from './../context/auth-context.js'
 
-const Guardian = () => (
-  <h1>
-    Guardian Route
-    <Auth/>
-  </h1>
-);
+const Guardian = () => {
+  const auth = useContext(AuthContext)
+  return (
+    <h1>
+      Guardian Route
+      {!auth.token && <Auth/>}
+      {auth.token && <LoremIpsum/>}
+      {auth.token && <button onClick={()=>auth.logout()} >signout</button>}
+    </h1>
+)};
 export default Guardian;
