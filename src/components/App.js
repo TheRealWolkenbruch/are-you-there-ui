2cef7f0d279c476a0ac8f2e2f681abb17731acf1
Omar Sotillo <omarsotillofranco@gmail.com>
2020-04-25 13:48:50 +0200

    feat: layout + styled components
    quite dirty

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import "./../css/App.css";
@@ -6,6 +7,58 @@ import Guardian from "./../routes/Guardian";
 import Ward from "./../routes/Ward";
 // https://medium.com/@danfyfe/using-react-context-with-functional-components-153cbd9ba214
 import { AuthContextProvider } from "./../context/auth-context.js";
+import Logo from "../assets/images/logo.svg";
+import { Layout } from "antd";
+
+const { Header, Content, Sider } = Layout;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  align-items: center;
+`;
+
+const LogoWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: baseline;
+  align-items: center;
+  background-color: #ffd8be;
+  padding: 1em;
+  width: 100%;
+  border-radius: 0 0 7px 7px;
+`;
+
+const Header1 = styled.h1`
+  font-size: 16px;
+  padding-left: 10px;
+  font-weight: 700;
+`;
+
+const StyledButton = styled.div`
+  margin-top: 50px;
+  background: #ffd8be;
+  border-radius: 7px;
+  padding-top: 5px;
+  padding-bottom: 5px;
+  padding-left: 25px;
+  padding-right: 25px;
+  font-weight: 600;
+  color: #002639;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+`;
+
+const ContentWrapper = styled.div`
+  background-color: white;
+  padding: 24;
+  height: 85vh;
+  padding: 100px;
+  margin: 40px 30px 40px 30px;
+  overflow: initial;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 11px;
+`;
 
 const App = () => {
   const [token, setToken] = useState(null);
@@ -26,16 +79,43 @@ const App = () => {
     login: login,
     logout: logout,
   };
+
   return (
     <Router>
-      <header>header</header>
-      <main>
-        <AuthContextProvider value={context}>
-          <Route exact path="/guardian" component={Guardian} />
-        </AuthContextProvider>
-        <Route path="/ward/:name" component={Ward} />
-      </main>
-      <footer>footer</footer>
+      <Layout>
+        <Sider
+          style={{
+            backgroundColor: "#002639",
+            overflow: "auto",
+            height: "100vh",
+            position: "fixed",
+            left: 0,
+          }}
+        >
+          <Container>
+            <LogoWrapper>
+              <img src={Logo} alt="Logo" />
+              <Header1>AreYouThere</Header1>
+            </LogoWrapper>
+            <StyledButton>Your wards</StyledButton>
+          </Container>
+        </Sider>
+        <Layout
+          theme="light"
+          className="site-layout"
+          style={{ marginLeft: 200 }}
+        >
+          <Header style={{ backgroundColor: "white" }}></Header>
+          <ContentWrapper>
+            <main>
+              <AuthContextProvider value={context}>
+                <Route exact path="/guardian" component={Guardian} />
+              </AuthContextProvider>
+              <Route path="/ward/:name" component={Ward} />
+            </main>
+          </ContentWrapper>
+        </Layout>
+      </Layout>
     </Router>
   );
 };
--- src/routes/Ward.js
+++ src/routes/Ward.js
@@ -1,11 +1,19 @@
 import React from "react";
 import BondsTable from "./../components/BondsTable";
+import styled from "styled-components";
+
+const Header1 = styled.h1`
+  font-size: 45px;
+  padding-left: 10px;
+  font-weight: 800;
+  margin-bottom: 50px;
+`;
 
 const Ward = ({ match }) => {
   const name = match.params.name;
   return (
     <>
-      <h1>The bonds for {name} ⏰</h1>
+      <Header1>The bonds for {name} ⏰</Header1>
       <BondsTable />
     </>
   );
