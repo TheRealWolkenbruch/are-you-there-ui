169fa73a98088ec63d4e3e072482272b416530d5
Thudilie <thuyanh.nguyen.de@gmail.com>
2020-04-25 10:27:05 +0200

    Structure directories, remove unused files

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './../css/App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Are You There
+      </header>
+    </div>
+  );
+}
+
+export default App;
