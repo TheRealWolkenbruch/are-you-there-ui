ddd1ea040b47f68d81ad24b56fe672293d554790
Thudilie <thuyanh.nguyen.de@gmail.com>
2020-04-25 16:23:13 +0200

    Remove strictmode because it doesn't play nice with antdesign

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './css/index.css';
-import App from './components/App';
-import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./css/index.css";
+import App from "./components/App";
+import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  //<React.StrictMode>
+  <App />,
+  //</React.StrictMode>
+  document.getElementById("root")
 );
