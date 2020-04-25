eb4e0303f77c59984e3dfb76ad52ce543bad29ab
Thudilie <thuyanh.nguyen.de@gmail.com>
2020-04-26 00:00:21 +0200

    Cleanup base

--- src/components/App.js
+++ src/components/App.js
@@ -13,54 +13,6 @@ import { Layout } from "antd";
 
 const { Header, Content, Sider } = Layout;
 
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  flex-wrap: wrap;
-  align-items: center;
-`;
-
-const LogoWrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: baseline;
-  align-items: center;
-  background-color: #ffd8be;
-  padding: 1em;
-  width: 100%;
-  border-radius: 0 0 7px 7px;
-`;
-
-const Header1 = styled.h1`
-  font-size: 16px;
-  padding-left: 10px;
-  font-weight: 700;
-`;
-
-const StyledButton = styled.div`
-  margin-top: 50px;
-  background: #ffd8be;
-  border-radius: 7px;
-  padding-top: 5px;
-  padding-bottom: 5px;
-  padding-left: 25px;
-  padding-right: 25px;
-  font-weight: 600;
-  color: #002639;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-`;
-
-const ContentWrapper = styled.div`
-  background-color: white;
-  padding: 24;
-  height: 85vh;
-  padding: 100px;
-  margin: 40px 30px 40px 30px;
-  overflow: initial;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  border-radius: 11px;
-`;
-
 const App = () => {
   const [token, setToken] = useState(null);
   const [email, setEmail] = useState(null);
@@ -84,37 +36,40 @@ const App = () => {
   return (
     <Router>
       <Layout>
-        <Sider
-          style={{
-            backgroundColor: "#002639",
-            overflow: "auto",
-            height: "100vh",
-            position: "fixed",
-            left: 0,
-          }}
-        >
-          <Container>
-            <LogoWrapper>
+        <Sider width={200} className="site-layout-background">
+          <div className="container">
+            <div className="logo_wrapper">
               <img src={Logo} alt="Logo" />
-              <Header1>AreYouThere</Header1>
-            </LogoWrapper>
+              <div className="logo">AreYouThere</div>
+            </div>
             {token && <GuardiansNav />}
-          </Container>
+          </div>
         </Sider>
-        <Layout
-          theme="light"
-          className="site-layout"
-          style={{ marginLeft: 200 }}
-        >
-          <Header style={{ backgroundColor: "white" }}></Header>
-          <ContentWrapper>
-            <main>
-              <AuthContextProvider value={context}>
-                <Route path="/guardian" component={Guardian} />
-              </AuthContextProvider>
-              <Route path="/ward/:name" component={Ward} />
-            </main>
-          </ContentWrapper>
+        <Layout>
+          <Header className="header">
+            <div className="logo" />
+            psdojaäspdj
+          </Header>
+          <Layout
+            className="site-layout-sub-header-background"
+            style={{ padding: "0 24px 24px" }}
+          >
+            <Content
+              className="site-layout-background"
+              style={{
+                padding: 24,
+                margin: 0,
+                minHeight: 280,
+              }}
+            >
+              <div className="content_wrapper">
+                <AuthContextProvider value={context}>
+                  <Route path="/guardian" component={Guardian} />
+                </AuthContextProvider>
+                <Route path="/ward/:name" component={Ward} />
+              </div>
+            </Content>
+          </Layout>
         </Layout>
       </Layout>
     </Router>
